0c0d597f302ca14d244dea299f8b4311084de45c
JovitoVinicius <viniciusjovito@gmail.com>
2022-11-20 10:40:05 -0300

    first commit

--- detran/settings.py
+++ detran/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'detran.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'detran',
+        'USER': 'detran',
+        'PASSWORD': 'detran',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
